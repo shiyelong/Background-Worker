8628ead57d052aa2dbc4907a5b0d60d7ece8bcfb
VirtualCeoi <756655151@qq.com>
2024-11-07 22:58:40 +0800

    我干哦，就写了个空的类  睡了

--- src/cpp/include/GameServer.h
+++ src/cpp/include/GameServer.h
@@ -1,1 +1,45 @@
-#include<ios>
+#pragma once
+#include<algorithm>
+#include<iostream>
+#include<sstream>
+#include<cstring>
+#include<winsock2.h>
+#include<ws2tcpip.h>
+
+class GameServer {
+public:
+    GameServer();
+
+    virtual ~GameServer() = 0;
+
+    bool Init();
+
+    void Run();
+
+    void Close();
+
+    void SendMessage(std::string msg);
+
+    void ReceiveMessage(std::string msg);
+
+private:
+    bool InitSocket();
+
+    void CloseSocket();
+
+    void SendSocket();
+
+    void ReceiveSocket();
+
+    void SendSocketData();
+
+    void ReceiveSocketData();
+
+    void SendMessageData();
+
+    void ReceiveMessageData();
+
+private:
+    SOCKET sock;
+    SOCKADDR_IN addr;
+};
