de5e78d661830c92adc7f9b4ce536f8ed7e073a2
VirtualCeoi <756655151@qq.com>
2024-11-10 15:59:54 +0800

    添加初始化函数逻辑

--- src/cpp/include/GameServer.h
+++ src/cpp/include/GameServer.h
@@ -1,22 +1,27 @@
 #pragma once
-#include<algorithm>
-#include<iostream>
-#include<sstream>
-#include<cstring>
-#include<winsock2.h>
-#include<ws2tcpip.h>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+
+#pragma comment(lib,"ws2_32.lib")
 
 class GameServer {
 public:
     GameServer();
 
-    virtual ~GameServer() = 0;
+    virtual ~GameServer();
 
     bool Init();
 
-    void Run();
+    void Run() const;
 
-    void Close();
+    void Close() const;
 
     void SendMessage(std::string msg);
 
--- src/cpp/src/GameServer.cpp
+++ src/cpp/src/GameServer.cpp
@@ -1,1 +1,45 @@
 #include "GameServer.h"
+
+GameServer::GameServer():sock(INVALID_SOCKET),addr() {
+    std::cerr<<"Creating GameServer"<<std::endl;
+}
+
+GameServer::~GameServer() {
+    std::cerr<<"Destroying GameServer"<<std::endl;
+}
+
+bool GameServer::Init() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return false;
+    }
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        return false;
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(9999);
+    if (connect(sock, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+        return false;
+    }
+    return true;
+}
+
+void GameServer::Run() const {
+    std::cerr<<"Running GameServer"<<std::endl;
+        int ret = listen(sock, 5);
+        if (ret == SOCKET_ERROR) {
+            std::cerr<<"Failed to listen"<<std::endl;
+            return;
+        }
+        std::cerr<<"Waiting for client connection"<<std::endl;
+}
+
+void GameServer::Close() const {
+    std::cerr<<"Closing GameServer"<<std::endl;
+    shutdown(sock, SD_BOTH);
+    closesocket(sock);
+    WSACleanup();
+}
+
+
